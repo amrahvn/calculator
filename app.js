506a5d7103823a61fc88bc26ec8f1c61778eb5b6
Amrahvn <amrahvn@code.edu.az>
2023-06-07 03:32:50 +0400

    animasiya

--- app.js
+++ app.js
@@ -23,32 +23,67 @@ btn.onclick=()=>{
 }
 
 function light(){
-    if(document.body.style.backgroundColor==="antiquewhite"){
-        document.body.style.backgroundColor="black"
-    }
-    else{
+    if(document.body.style.backgroundColor==="black"){
         document.body.style.backgroundColor="antiquewhite"
     }
+    else{
+        document.body.style.backgroundColor="black"
+    }
 }
 
-let now = new Date();
-let hour = now.getHours()
+// let now = new Date();
+// let hour = now.getHours()
 
-if (hour >= 6 && hour < 12) {
-    alert('Good morning')
-    console.log("Good morning");
-    document.body.style.backgroundColor='white'
+// if (hour >= 6 && hour < 12) {
+//     alert('Good morning')
+//     console.log("Good morning");
+//     document.body.style.backgroundColor='white'
     
     
-} else if (hour >= 12 && hour < 18) {
-    alert('Good afternoon')
-    console.log("Good day");
-    document.body.style.backgroundColor='#fbe35f'
+// } else if (hour >= 12 && hour < 18) {
+//     alert('Good afternoon')
+//     console.log("Good day");
+//     document.body.style.backgroundColor='#fbe35f'
   
     
-} else {
-    alert('Good night')
-    console.log("Good night");
-    document.body.style.backgroundColor='black'   
+// } else {
+//     alert('Good night')
+//     console.log("Good night");
+//     document.body.style.backgroundColor='black'   
    
+// }
+
+
+window.addEventListener('load', function() {
+    var letter = document.querySelector('.letter');
+    letter.style.opacity = '0';
+    letter.style.transform = 'scale(0)';
+
+    setTimeout(function() {
+        letter.style.opacity = '1';
+        letter.style.transform = 'scale(1)';
+    }, 1000);
+});
+
+var animatedText = document.getElementById("animatedText");
+
+
+animatedText.classList.add("animated");
+
+
+animatedText.addEventListener("animationiteration", function() {
+    this.style.color = getRandomColor();
+});
+
+
+function getRandomColor() {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
+
+var circle = document.querySelector('.circle');
+
