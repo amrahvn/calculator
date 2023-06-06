1b34a9369602a1cd00c769553d3a353e58d23f90
Amrahvn <amrahvn@code.edu.az>
2023-06-07 02:10:10 +0400

    update2

--- app.js
+++ app.js
@@ -17,3 +17,34 @@ btn.onclick=()=>{
    document.getElementById('um').innerHTML="amount to be paid:"+smebleg+"Azn" 
    document.getElementById('res').innerHTML= "monthly payment:"+ ayliqodeme+"Azn" 
 }
+
+function light(){
+    if(document.body.style.backgroundColor==="antiquewhite"){
+        document.body.style.backgroundColor="black"
+    }
+    else{
+        document.body.style.backgroundColor="antiquewhite"
+    }
+}
+
+let now = new Date();
+let hour = now.getHours()
+
+if (hour >= 6 && hour < 12) {
+    alert('Good morning')
+    console.log("Good morning");
+    document.body.style.backgroundColor='white'
+    
+    
+} else if (hour >= 12 && hour < 18) {
+    alert('Good afternoon')
+    console.log("Good day");
+    document.body.style.backgroundColor='#fbe35f'
+  
+    
+} else {
+    alert('Good night')
+    console.log("Good night");
+    document.body.style.backgroundColor='black'   
+   
+}
