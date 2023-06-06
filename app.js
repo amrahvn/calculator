28e5b277f9e79519520c597724df510ff3ad237b
Amrahvn <amrahvn@code.edu.az>
2023-06-07 01:54:06 +0400

    update

--- app.js
+++ app.js
@@ -4,15 +4,16 @@ let btn=document.getElementById('calc')
 btn.onclick=()=>{
   
 
- let num1=parseInt(document.getElementById('amount').value)
-let num2=parseInt(document.getElementById('percentage').value+"%")
-let num3=parseInt(document.getElementById('monthly').value)
+     let num1=parseInt(document.getElementById('amount').value)
+     let num2=parseInt(document.getElementById('percentage').value+"%")
+     let num3=parseInt(document.getElementById('monthly').value)
 
 
 
-let mebleg =num1*num2/100;
-let smebleg=num1+mebleg;
-let ayliqodeme=smebleg/num3
+     let mebleg =num1*num2/100;
+     let smebleg=num1+mebleg;
+     let ayliqodeme=smebleg/num3
 
-   document.getElementById('res').innerHTML= "monthly payment:"+ ayliqodeme
+   document.getElementById('um').innerHTML="amount to be paid:"+smebleg+"Azn" 
+   document.getElementById('res').innerHTML= "monthly payment:"+ ayliqodeme+"Azn" 
 }
